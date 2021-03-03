f4b8dace53993483d327da48759329cc740efaa2
mamohamed252 <ma.mohamed252@gmail.com>
2021-03-02 20:37:14 -0600

    Started SBA requirement 2

--- Practice/Solution/src/main/java/com/example/Item.java
+++ Practice/Solution/src/main/java/com/example/Item.java
@@ -2,14 +2,62 @@ package com.example;
 
 public class Item{
     
-    
+	// properties
+	// requirement 1
+	private String itemName;
+	private String itemDesc;
+	private double itemPrice;
+	private int quantity;
+	private int availableQuantity;
+		
+	// default constructor
     public Item() {
         // Your code here
+    	this.quantity = 1;
     }
-    
+    // specified constructor
     public Item(String itemName, String itemDesc, Double itemPrice, Integer availableQuantity) {
         // Your code here
+    	this.itemName = itemName;
+    	this.itemDesc = itemDesc;
+    	this.itemPrice = itemPrice;
+    	this.availableQuantity = availableQuantity;	
     }
+    
+    // getters and setters for items
+    public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public String getItemDesc() {
+		return itemDesc;
+	}
+	public void setItemDesc(String itemDesc) {
+		this.itemDesc = itemDesc;
+	}
+	public double getItemPrice() {
+		return itemPrice;
+	}
+	public void setItemPrice(double itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getAvailableQuantity() {
+		return availableQuantity;
+	}
+	public void setAvailableQuantity(int availableQuantity) {
+		this.availableQuantity = availableQuantity;
+	}
+    
+    
+    
     
     
     
--- Practice/Solution/src/main/java/com/example/TheSystem.java
+++ Practice/Solution/src/main/java/com/example/TheSystem.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 
 public abstract class TheSystem {
    
-
+	private HashMap<String, Item> itemCollection;
+	
     TheSystem() {
         // Your code here
     }
@@ -18,10 +19,27 @@ public abstract class TheSystem {
     
     public Boolean checkAvailability(Item item) {
         // Your code here
+    	if(item.getQuantity() >= item.getAvailableQuantity()) {
+    		System.out.println("System is unable to add " + item.getItemName() + " to the card. System only has " + item.getAvailableQuantity() + " " + item.getItemName() + "s.");
+    		return false;
+    	}else {
+    		return true;
+    	}
     }
     
     public Boolean add(Item item) {
         // Your code here
+    	if (item == null) {
+			return false;
+		} if(checkAvailability(item)) {
+			item.setQuantity(item.getQuantity() + 1);
+			return true;
+		} if(checkAvailability(item)) {
+			
+			return true;
+		}else {
+			return false;
+		}
     }
 
     public Item remove(String itemName) {
